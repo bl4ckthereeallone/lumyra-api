3b7eeab7f0754800629dcff88cbdda4d3d3420f8
TheRealBl4ck._ <kebabxciccio@gmail.com>
2026-03-20 21:12:50 +0100

    Update generate-key.js

--- api/generate-key.js
+++ api/generate-key.js
@@ -9,7 +9,7 @@ const WINDOW = 24 * 60 * 60 * 1000;
 module.exports = async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, X-Request-Token");
   if (req.method === "OPTIONS") return res.status(200).end();
   if (req.method !== "POST") return res.status(405).end();
 
